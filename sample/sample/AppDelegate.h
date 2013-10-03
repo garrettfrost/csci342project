c7f4030d2879fbbe7076a10b6c41ab2a3fbdb9db
RileyAdam <rolaeo@hotmail.com>
2013-10-03 12:59:54 +1000

    sample

---
+++ sample/sample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  sample
+//
+//  Created by cs321kw1a on 3/10/13.
+//  Copyright (c) 2013 rjda60. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ sample/sample/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  sample
+//
+//  Created by cs321kw1a on 3/10/13.
+//  Copyright (c) 2013 rjda60. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
