5f02dfc3d3b0bce37b53987dabc907cc03c95c0e
cs321kw1a <cs321kw1a@proj-mac3.cs.uow.edu.au>
2013-10-03 22:53:52 +1000

    Initial Commit

---
+++ studentHelperApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  studentHelperApp
+//
+//  Created by cs321kw1a on 3/10/13.
+//  Copyright (c) 2013 gf99rjda640. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ studentHelperApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  studentHelperApp
+//
+//  Created by cs321kw1a on 3/10/13.
+//  Copyright (c) 2013 gf99rjda640. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
