f80a7d69164e727091fdec7dc16e3a0b5bda6062
cs321kw1a <cs321kw1a@proj-mac3.cs.uow.edu.au>
2013-10-03 23:56:24 +1000

    core data model added

---
+++ Subjects.h
@@ -0,0 +1,37 @@
+//
+//  Subjects.h
+//  studentHelperApp
+//
+//  Created by cs321kw1a on 3/10/13.
+//  Copyright (c) 2013 gf99rjda640. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Subjects : NSManagedObject
+
+@property (nonatomic, retain) NSString * name;
+@property (nonatomic, retain) NSString * subjectCode;
+@property (nonatomic, retain) NSString * session;
+@property (nonatomic, retain) NSNumber * creditPoints;
+@property (nonatomic, retain) NSNumber * mark;
+@property (nonatomic, retain) NSNumber * completed;
+@property (nonatomic, retain) NSSet *assessmentRelationship;
+@property (nonatomic, retain) NSSet *classesRelationship;
+@end
+
+@interface Subjects (CoreDataGeneratedAccessors)
+
+- (void)addAssessmentRelationshipObject:(NSManagedObject *)value;
+- (void)removeAssessmentRelationshipObject:(NSManagedObject *)value;
+- (void)addAssessmentRelationship:(NSSet *)values;
+- (void)removeAssessmentRelationship:(NSSet *)values;
+
+- (void)addClassesRelationshipObject:(NSManagedObject *)value;
+- (void)removeClassesRelationshipObject:(NSManagedObject *)value;
+- (void)addClassesRelationship:(NSSet *)values;
+- (void)removeClassesRelationship:(NSSet *)values;
+
+@end
---
+++ Subjects.m
@@ -0,0 +1,23 @@
+//
+//  Subjects.m
+//  studentHelperApp
+//
+//  Created by cs321kw1a on 3/10/13.
+//  Copyright (c) 2013 gf99rjda640. All rights reserved.
+//
+
+#import "Subjects.h"
+
+
+@implementation Subjects
+
+@dynamic name;
+@dynamic subjectCode;
+@dynamic session;
+@dynamic creditPoints;
+@dynamic mark;
+@dynamic completed;
+@dynamic assessmentRelationship;
+@dynamic classesRelationship;
+
+@end
